3e466a0d30bf04b94251d537c83fa52397df0895
Romeo Roman <33218893+romeoroman@users.noreply.github.com>
2019-10-18 22:24:57 +0300

    Update ValuesController.cs
    Test SSH

--- FirstTest/Controllers/ValuesController.cs
+++ FirstTest/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace FirstTest.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/values/1
         public string Get(int id)
         {
             return "value";
